07fad420c2b2350dc42c1c6871864bdc9eacdc9a
lakshay9131 <41942751+lakshay9131@users.noreply.github.com>
2023-01-23 11:38:50 +0530

    //can be solved by using [.... this.state.words] here

--- App.tsx
+++ App.tsx
@@ -10,9 +10,24 @@ export default function App() {
     </div>
   );
 }
-class ListOfWords extends React.PureComponent {
+class ListOfWords extends React.Component {
   render() {
-    return <div>{this.props.words.join(',')}</div>;
+    return (
+      <div>
+        <h2>List1 </h2>
+        {this.props.words.join(',')}
+      </div>
+    );
+  }
+}
+class ListOfWords2 extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <h2>List2(Pure) </h2>
+        {this.props.words.join(',')}
+      </div>
+    );
   }
 }
 
@@ -27,7 +42,8 @@ class WordAdder extends React.Component {
 
   handleClick() {
     // This section is bad style and causes a bug
-    const words = [...this.state.words];
+    const words = this.state.words;
+    //can be solved by using [.... this.state.words] here
     words.push('marklar');
     this.setState({ words: words });
   }
@@ -37,7 +53,10 @@ class WordAdder extends React.Component {
       <div>
         <button onClick={this.handleClick}>click</button>
         <ListOfWords words={this.state.words} />
-        {this.state.words.length} {this.state.words.join(',')}
+        <ListOfWords2 words={this.state.words} />
+        <h2>Orignal List </h2>
+        length {this.state.words.length} <h5>list </h5>
+        {this.state.words.join(',')}
       </div>
     );
   }
